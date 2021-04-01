f72e6af462f171f91ba7d19d565c0fc5c8b26d89
Andrew Theismann <andrew@theismannconsulting.com>
2021-04-01 17:18:43 -0500

    fix user addition

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -60,18 +60,21 @@ class App extends Component {
               />
             }/>
 
-            {/* <Route exact path='/signup' render={({ history }) => 
-              <UserEditPage
-                history={history}
-                handleSignupOrLogin={this.handleSignupOrLogin}
-              />
-            }/> */}
-
             <Route exact path='/login' render={({ history }) => 
               <LoginPage
                 history={history}
                 handleSignupOrLogin={this.handleSignupOrLogin}
               />
+            }/>
+
+            <Route exact path='/signup' render={({ history }) => 
+              this.state.user.role === 'Admin' ?
+              <UserEditPage
+                history={history}
+                handleSignupOrLogin={this.handleSignupOrLogin}
+              />
+              :
+                <Redirect to='/login'/>
             }/>
 
             <Route exact path='/users' render={({ history }) => (
